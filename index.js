c994cc8cb86d7e82d4ae8ec781bd745b2878a22e
Noah <noahhughesph@gmail.com>
2023-07-05 23:44:57 -0400

    done

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = [];
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversedWord.push(word[i]);
+  }
+  const newWord = reversedWord.join('');
+  if (newWord === word) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
@@ -13,13 +22,13 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('racecar'));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log('Expecting: false');
+  console.log('=>', isPalindrome('robot'));
 }
 
 module.exports = isPalindrome;
